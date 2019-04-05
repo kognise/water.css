5ae08050adcbce28135614ce5234b7ec2ee113c6
Kognise <felix.mattick@gmail.com>
2019-04-05 09:21:05 -0400

    Update theme switching code

--- docs/script.js
+++ docs/script.js
@@ -1,11 +1,11 @@
 document.getElementById('switch').addEventListener('click', () => {
   const { body } = document
   const ph = document.getElementById('ph')
-  if (body.className) {
-    body.className = ''
+  if (body.classList.contains('light')) {
+    body.classList.remove('light')
     ph.src = ph.src.replace('theme=light', 'theme=dark')
   } else {
-    body.className = 'light'
+    body.classList.add('light')
     ph.src = ph.src.replace('theme=dark', 'theme=light')
   }
 })
