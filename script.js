54d5cbd805f3cf1f263d58a3b8722373d1a7718b
Kognise <felix.mattick@gmail.com>
2019-04-06 15:07:11 -0400

    Restructure everything

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 document.getElementById('switch').addEventListener('click', () => {
   const stylesheet = document.getElementById('stylesheet')
-  if (stylesheet.getAttribute('href') === 'dist/water-dark.css') {
-    stylesheet.setAttribute('href', 'dist/water-light.css')
+  if (stylesheet.getAttribute('href') === 'dist/dark.css') {
+    stylesheet.setAttribute('href', 'dist/light.css')
   } else {
-    stylesheet.setAttribute('href', 'dist/water-dark.css')
+    stylesheet.setAttribute('href', 'dist/dark.css')
   }
 })
