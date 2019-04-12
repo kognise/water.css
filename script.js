96d7db579dd4ac0755e3f979e41c98700f3de3dc
Kyle Pollard <kylejrp@gmail.com>
2019-04-11 22:54:59 -0700

    Added .min.css files to dist Made .css contain human readable styles

--- script.js
+++ script.js
@@ -1,11 +1,8 @@
 document.getElementById('switch').addEventListener('click', () => {
   const stylesheet = document.getElementById('stylesheet')
-  const ph = document.getElementById('ph')
-  if (stylesheet.getAttribute('href') === 'dist/dark.css') {
-    stylesheet.setAttribute('href', 'dist/light.css')
-    ph.src = ph.src.replace('theme=dark', 'theme=light')
+  if (stylesheet.getAttribute('href') === 'dist/dark.min.css') {
+    stylesheet.setAttribute('href', 'dist/light.min.css')
   } else {
-    stylesheet.setAttribute('href', 'dist/dark.css')
-    ph.src = ph.src.replace('theme=light', 'theme=dark')
+    stylesheet.setAttribute('href', 'dist/dark.min.css')
   }
 })
