d28c01b7ba7e68b2f2cd53750ed03617fd08a1df
Kognise <felix.mattick@gmail.com>
2019-04-22 20:18:36 -0400

    Re-add stackoverflow attribution

--- gulpfile.js
+++ gulpfile.js
@@ -20,6 +20,7 @@ const paths = {
   }
 }
 
+// https://stackoverflow.com/a/20732091
 function humanFileSize(size) {
   var i = Math.floor(Math.log(size) / Math.log(1024));
   return (size / Math.pow(1024, i)).toFixed(2) * 1 + ' ' + [ 'B', 'kB', 'MB', 'GB', 'TB' ][i]
