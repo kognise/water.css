413e18982124a0594cdd9beed6d550d527feb804
Jonas Kuske <30421456+jonaskuske@users.noreply.github.com>
2019-05-06 00:24:42 +0200

    feat: update gulpfile to compile CSS variables

--- gulpfile.js
+++ gulpfile.js
@@ -9,10 +9,18 @@ const browserSync = require('browser-sync').create()
 const chalk = require('chalk');
 const rename = require('gulp-rename');
 const filter = require('gulp-filter');
+const flatten = require('gulp-flatten')
+const postcssSassParser = require('postcss-scss')
+const postcssCssVariables = require('postcss-css-variables')
 
 const paths = {
   styles: {
     src: 'src/**/*.scss',
+    variables: {
+      src: 'src/variables-*.scss',
+      compiled: 'src/_variables/_variables-*.scss',
+      dest: 'src/_variables',
+    },
     dest: 'dist'
   },
   html: {
@@ -22,64 +30,104 @@ const paths = {
 
 // https://stackoverflow.com/a/20732091
 function humanFileSize(size) {
-  var i = Math.floor( Math.log(size) / Math.log(1024) );
-  return ( size / Math.pow(1024, i) ).toFixed(2) * 1 + ' ' + ['B', 'kB', 'MB', 'GB', 'TB'][i];
+  var i = Math.floor(Math.log(size) / Math.log(1024));
+  return (size / Math.pow(1024, i)).toFixed(2) * 1 + ' ' + ['B', 'kB', 'MB', 'GB', 'TB'][i];
 };
 
 function formatByteMessage(source, data) {
-  const change = (data.savings > 0 ? 'saved' : 'gained')
-  const prettySavings = humanFileSize(Math.abs(data.savings))
   const prettyStartSize = humanFileSize(data.startSize)
-  let prettyEndSize = humanFileSize(data.endSize)
+  let message = '';
 
-  if (data.endSize > data.startSize) {
-    prettyEndSize = chalk.yellow(prettyEndSize)
-  }
+  if (data.startSize !== data.endSize) {
+    const change = (data.savings > 0 ? 'saved' : 'gained')
+    const prettySavings = humanFileSize(Math.abs(data.savings))
+    let prettyEndSize = humanFileSize(data.endSize)
 
-  if (data.endSize < data.startSize) {
-    prettyEndSize = chalk.green(prettyEndSize)
-  }
+    if (data.endSize > data.startSize) prettyEndSize = chalk.yellow(prettyEndSize)
+    if (data.endSize < data.startSize) prettyEndSize = chalk.green(prettyEndSize)
 
-  return `${chalk.cyan(source.padStart(12, ' '))}: ${data.fileName} ${change} ${prettySavings} (${prettyStartSize} -> ${prettyEndSize})`
+    message = chalk`${change} ${prettySavings} (${prettyStartSize} -> {bold ${prettyEndSize}})`
+  } else message = chalk`kept original filesize. ({bold ${prettyStartSize}})`
+
+  return chalk`{cyan ${(source.padStart(12, ' '))}}: {bold ${data.fileName}} ${message}`
 }
 
-function style() {
+/* Inlines variable references within the variable files themselves. */
+/* Allows computing new variables based on previous ones, e.g. with `lighten()` */
+function computeVariables() {
+  const plugins = [postcssCssVariables({ preserve: 'computed' })]
+  const parser = postcssSassParser
+
+  return gulp.src(paths.styles.variables.src)
+    .pipe(postcss(plugins, { parser }))
+    .pipe(rename({ prefix: '_' }))
+    .pipe(gulp.dest(paths.styles.variables.dest));
+}
+
+function compileStyles() {
+  const isLegacyOrStandalone = path => /standalone|legacy/.test(path)
+
+  const excludeModern = filter(file => isLegacyOrStandalone(file.path), { restore: true })
+  const excludeLegacy = filter(file => !isLegacyOrStandalone(file.path), { restore: true })
+
   return (
-    gulp.src(paths.styles.src)
-        // Add sourcemaps
-        .pipe(sourcemaps.init())
-        // Create a human readable sass file
-        .pipe(sass({outputStyle: 'expanded'}))  
-        // Catch any sass errors
-        .on('error', sass.logError)
-        // Calculate size before autoprefixing
-        .pipe(bytediff.start()) 
-        // autoprefix
-        .pipe(postcss([ autoprefixer()])) 
-        // Write the amount gained by autoprefixing
-        .pipe(bytediff.stop((data) => formatByteMessage('autoprefixer', data))) 
-        // Write the sourcemaps after making pre-minified changes
-        .pipe(sourcemaps.write('.'))
-        // Write pre-minified styles
-        .pipe(gulp.dest(paths.styles.dest))
-        // Remove sourcemaps from the pipeline, only keep css
-        .pipe(filter('**/*.css'))
-        // Calculate size before minifying
-        .pipe(bytediff.start()) 
-        // Minify using cssnano
-        .pipe(postcss([cssnano()]))
-        // Write the amount saved by minifying
-        .pipe(bytediff.stop((data) => formatByteMessage('cssnano', data)))
-        // Rename the files have the .min suffix
-        .pipe(rename({suffix: '.min' }))
-        // Write the sourcemaps after making all changes
-        .pipe(sourcemaps.write('.'))
-        // Write the minified files
-        .pipe(gulp.dest(paths.styles.dest))
-        // Stream any changes to browserSync
-        .pipe(browserSync.stream())
+    gulp.src(paths.styles.src, { ignore: paths.styles.variables.src })
+      // Add sourcemaps
+      .pipe(sourcemaps.init())
+      // Create a human readable sass file
+      .pipe(sass({ outputStyle: 'expanded' }))
+      // Catch any sass errors
+      .on('error', sass.logError)
+
+      // * Process legacy & standalone builds *
+      .pipe(excludeModern)
+      // Inline variable values so CSS works in legacy browsers
+      .pipe(postcss([postcssCssVariables()]))
+      // Calculate size before autoprefixing
+      .pipe(bytediff.start())
+      // autoprefix
+      .pipe(postcss([autoprefixer()]))
+      // Write the amount gained by autoprefixing
+      .pipe(bytediff.stop((data) => formatByteMessage('autoprefixer', data)))
+      .pipe(excludeModern.restore)
+
+      // * Process modern builds *
+      .pipe(excludeLegacy)
+      // Calculate size before autoprefixing
+      .pipe(bytediff.start())
+      // autoprefix modern builds
+      // TODO: Use separate browserslist to only apply prefixes needed in *modern* browsers
+      .pipe(postcss([autoprefixer()]))
+      // Write the amount gained by autoprefixing
+      .pipe(bytediff.stop((data) => formatByteMessage('autoprefixer', data)))
+      .pipe(excludeLegacy.restore)
+
+      // Write the sourcemaps after making pre-minified changes
+      .pipe(sourcemaps.write('.'))
+      // Flatten output so files end up in dist/*, not dist/builds/*
+      .pipe(flatten())
+      // Write pre-minified styles
+      .pipe(gulp.dest(paths.styles.dest))
+      // Remove sourcemaps from the pipeline, only keep css
+      .pipe(filter('**/*.css'))
+      // Calculate size before minifying
+      .pipe(bytediff.start())
+      // Minify using cssnano
+      .pipe(postcss([cssnano()]))
+      // Write the amount saved by minifying
+      .pipe(bytediff.stop((data) => formatByteMessage('cssnano', data)))
+      // Rename the files have the .min suffix
+      .pipe(rename({ suffix: '.min' }))
+      // Write the sourcemaps after making all changes
+      .pipe(sourcemaps.write('.'))
+      // Write the minified files
+      .pipe(gulp.dest(paths.styles.dest))
+      // Stream any changes to browserSync
+      .pipe(browserSync.stream())
   )
 }
+
+const style = gulp.series(computeVariables, compileStyles)
 
 function reload() {
   browserSync.reload()
@@ -95,7 +143,10 @@ function watch() {
     startPath: 'index.html'
   })
 
-  gulp.watch(paths.styles.src, style)
+  // Don't watch compiled variables or every build triggers the watcher again (infinite loop)
+  const watched = [paths.styles.src, `!${paths.styles.variables.compiled}`]
+
+  gulp.watch(watched, style)
   gulp.watch(paths.html.src, reload)
 }
 
