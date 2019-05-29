7712f6aab9aa469ce1cdfa247ade03a1b9432878
kimulaco <kmrkdev@gmail.com>
2019-05-29 15:40:45 +0900

    Update switch theme script

--- script.js
+++ script.js
@@ -1,4 +1,36 @@
-document.getElementById('switch').addEventListener('click', function() {
+(function () {
+  const switchBtn = document.getElementById('switch');
   const stylesheet = document.getElementById('stylesheet');
-  stylesheet.href = stylesheet.href.replace(/dark|light/, function(replaced) { return replaced === 'dark' ? 'light' : 'dark' });
-});
+  const mql = matchMedia('(prefers-color-scheme: dark)');
+  const themes = {
+    dark: '../dist/dark.css',
+    darkStandalone: '../dist/dark.standalone.css',
+    light: '../dist/light.css',
+    lightStandalone: '../dist/light.standalone.css'
+  };
+
+  switchBtn.addEventListener('click', function() {
+    switch (stylesheet.getAttribute('href')) {
+      case themes.dark:
+        stylesheet.href = mql.matches ? themes.lightStandalone : themes.darkStandalone;
+        break;
+      case themes.darkStandalone:
+        stylesheet.href = themes.lightStandalone;
+        break;
+      case themes.light || themes.lightStandalone:
+        stylesheet.href = mql.matches ? themes.lightStandalone : themes.darkStandalone;
+        break;
+      case themes.lightStandalone:
+        stylesheet.href = themes.darkStandalone;
+        break;
+    }
+  });
+
+  mql.addListener(function (mql) {
+    if (mql.matches) {
+      stylesheet.href = themes.dark;
+    } else {
+      stylesheet.href = themes.light;
+    }
+  })
+})();
