4011816d590e4fda19af2779506d9f3248ce5e41
chathuka_fernando <38041960+chathukafernando@users.noreply.github.com>
2019-10-22 19:23:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
 const gulpfile = require('./gulpfile')
 gulpfile.watch()
+
+
+<!DOCTYPE HTML>
+<html>
+
+<body>
+
+  <p>Before the script...</p>
+
+  <script>
+    alert( 'Hello, world!' );
+  </script>
+
+  <p>...After the script.</p>
+
+</body>
+
+</html>
