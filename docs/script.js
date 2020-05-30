827691d5513ea52dfbc5cfc403ee28597a6ee548
Jonas Kuske <30421456+jonaskuske@users.noreply.github.com>
2020-05-30 05:16:45 +0200

    fix: allow overriding product hunt theme

--- docs/script.js
+++ docs/script.js
@@ -24,8 +24,8 @@ const table = {
 
 const prefersColorScheme = window.matchMedia('(prefers-color-scheme: light)')
 
-const updateProductHunt = () => {
-  const theme = prefersColorScheme.matches ? 'light' : 'dark'
+const updateProductHunt = (theme) => {
+  theme = theme || (prefersColorScheme.matches ? 'light' : 'dark')
   productHunt.src = `https://api.producthunt.com/widgets/embed-image/v1/top-post-badge.svg?post_id=150490&theme=${theme}&period=daily`
 }
 
@@ -44,14 +44,14 @@ const updateTheme = () => {
   table.fileName.innerText = fileName
   table.fileSize.innerText = `${fileSizes[theme].toFixed(2)} kb`
 
-  updateProductHunt()
-
   if (theme === 'auto') {
+    updateProductHunt()
     table.theme.innerHTML = `
     Respects user-defined theme settings using <a style="--links: var(--code)" href="https://developer.mozilla.org/en-US/docs/Web/CSS/@media/prefers-color-scheme" target="_blank" rel="noopener"><code>prefers-color-scheme</code></a>.<br>
     Light in browsers where the theme settings can't be detected.
     `
   } else {
+    updateProductHunt(theme)
     table.theme.innerText = `Theme is forced to ${theme}.`
   }
 }
