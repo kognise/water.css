0859ec67e67bd0a7aed296174c7f098ce6fa0364
HarshitJoshi9152 <harsjoshi1614@gmail.com>
2021-08-21 01:13:25 +0530

    added icons & fix !

--- bookmarklet/original.js
+++ bookmarklet/original.js
@@ -22,18 +22,6 @@ document.head.append(
   })
 )
 
-const toggleTheme = () => {
-  const rootElm = document.querySelector(':root')
-  // var rs = getComputedStyle(rootElm);
-  const theme = rootElm.getAttribute('theme')
-
-  if (theme === 'dark') {
-    rootElm.setAttribute('theme', 'light')
-  } else {
-    rootElm.setAttribute('theme', 'dark')
-  }
-}
-
 // css required to support theme switching !
 const themeCSS = `/* * light theme colors */
 :root[theme="light"] {
@@ -92,10 +80,30 @@ const themeCSS = `/* * light theme colors */
 document.body.append(
   createElement('style', {
     innerText: themeCSS
-  }),
-  createElement('button', {
-    innerText: 'toggle theme !',
-    style: 'position: fixed !important; top: 50px !important; right: 100px',
-    onclick: toggleTheme
   })
 )
+
+const moonSVG = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-moon"><path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path></svg>'
+const sunSVG = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-sun"><circle cx="12" cy="12" r="5"></circle><line x1="12" y1="1" x2="12" y2="3"></line><line x1="12" y1="21" x2="12" y2="23"></line><line x1="4.22" y1="4.22" x2="5.64" y2="5.64"></line><line x1="18.36" y1="18.36" x2="19.78" y2="19.78"></line><line x1="1" y1="12" x2="3" y2="12"></line><line x1="21" y1="12" x2="23" y2="12"></line><line x1="4.22" y1="19.78" x2="5.64" y2="18.36"></line><line x1="18.36" y1="5.64" x2="19.78" y2="4.22"></line></svg>'
+
+const toggleBtn = createElement('div', {
+  innerHTML: sunSVG,
+  style: 'position: fixed !important; top: 50px !important; right: 100px; cursor: pointer;'
+})
+
+const toggleTheme = () => {
+  const rootElm = document.querySelector(':root')
+  const theme = rootElm.getAttribute('theme')
+
+  if (theme === 'light' || theme === null) {
+    rootElm.setAttribute('theme', 'dark')
+    toggleBtn.innerHTML = moonSVG
+  } else {
+    rootElm.setAttribute('theme', 'light')
+    toggleBtn.innerHTML = sunSVG
+  }
+}
+
+toggleBtn.onclick = toggleTheme
+
+document.body.append(toggleBtn)
