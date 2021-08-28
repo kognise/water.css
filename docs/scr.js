e35297158d650687f68a945f90cf139baf42409e
Ajay Krupal K <67155124+ajaykrupalk@users.noreply.github.com>
2021-08-28 21:20:36 +0530

    Delete scr.js

--- docs/scr.js
+++
@@ -1,9 +0,0 @@
-function myFunction(element) {
-    /* Get the field */
-    var copyText = document.getElementById(element);
-    console.log('help');
-    navigator.clipboard.writeText(copyText.innerHTML);
-  
-    /* Alert the copied text */
-    alert("Copied!");
-}
